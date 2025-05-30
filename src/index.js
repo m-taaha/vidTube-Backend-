cfdcfdae2ab96596951c58f24478b2f81165046b
Mohammad Taaha Ashraf <mohammadtaahaashraf@gmail.com>
2025-05-30 14:12:18 +0530

    added utitlities

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({
   path: "./.env",
 });
 
-connectDB();
+const PORT = process.env.PORT || 8000;
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server is running at port ${PORT}`);
+    });
+  } catch (error) {
+    console.log("MongoDB connection failed!!!", error);
+    process.exit(1);
+  }
+};
+
+startServer();
