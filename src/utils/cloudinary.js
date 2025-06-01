07476c3561b8237e86d53256a5781cd3731f7f2d
Mohammad Taaha Ashraf <mohammadtaahaashraf@gmail.com>
2025-06-01 13:03:57 +0530

    added multer and cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+// Configuration
+cloudinary.config({
+  cloud_name: "CLOUDINARY_CLOUD_NAME",
+  api_key: "CLOUDINARY_API_KEY",
+  api_secret: "CLOUDINARY_API_SECRET",
+});
+
+const uploadonCloudinary = async (localFilePath) => {
+  try {
+    if (!loacalPath) return null;
+    //upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_tupe: "auto",
+    });
+    //file has been uploaded successfully
+    console.log("file is uploaded on cloudinary", response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); //remove the locally saved temporary file as the upload operation got failed
+    return null;
+  }
+};
+
+export { uploadonCloudinary };
